0a000830660ac9dd151e69aabea673d438945a83
Scott Woyak <scott.woyak@gmail.com>
2020-10-01 21:10:01 -0400

    removed 'webpack' from the initial browser title

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
    // add to webpack plugins
    plugins: [
       new HtmlWebpackPlugin({
+         title: '',
          // needed to prevent iphones from zooming in when editing input fields
          meta: { viewport: 'user-scalable=0' }
       })
