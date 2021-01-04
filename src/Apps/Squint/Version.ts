e6d155fc5eb99f7e7d27980a599b6116318e85b6
Scott Woyak <scott.woyak@gmail.com>
2021-01-03 21:24:17 -0500

    build 98

--- src/Apps/Squint/Version.ts
+++ src/Apps/Squint/Version.ts
@@ -1,7 +1,7 @@
 export class Version {
    public static Major = 0;
    public static Minor = 0;
-   public static Build = '97';
+   public static Build = '98';
 
    public static toString(): string {
       return this.Major + '.' + this.Minor + '.' + this.Build;
